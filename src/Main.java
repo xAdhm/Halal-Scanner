9b8fc31fbfe3a9951f7ebb4e2bd5650edc6780d8
Adham Mustafa <adhoomy.mustafa@gmail.com>
2026-04-17 20:53:55 -0400

    added Product class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Ingredient ingredient1 = new Ingredient("Chicken", PermissibilityStatus.HALAL);
-        Ingredient ingredient2 = new Ingredient("Pork", PermissibilityStatus.HARAM);
-        Ingredient ingredient3 = new Ingredient("Gelatin", PermissibilityStatus.DOUBTFUL);
+        // create some ingredients
+        Ingredient chicken = new Ingredient("Chicken", PermissibilityStatus.HALAL);
+        Ingredient salt = new Ingredient("Salt", PermissibilityStatus.HALAL);
+        Ingredient gelatin = new Ingredient("Gelatin", PermissibilityStatus.HARAM);
 
-        System.out.println(ingredient1);
-        System.out.println(ingredient2);
-        System.out.println(ingredient3);
+        // create a product with an ingredients list
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(chicken);
+        ingredients.add(salt);
+
+        Product product = new Product("Chicken Chips", ingredients);
+
+        // test toString and default status
+        System.out.println(product);
+        System.out.println("Is halal: " + product.isHalal());
+
+        // test addIngredient
+        product.addIngredient(gelatin);
+        System.out.println("After adding gelatin: " + product.getIngredients());
+
+        // test removeIngredient
+        product.removeIngredient(gelatin);
+        System.out.println("After removing gelatin: " + product.getIngredients());
+
+        // test setStatus
+        product.setStatus(PermissibilityStatus.HALAL);
+        System.out.println("After setting HALAL: " + product.isHalal());
     }
 
 }
---
+++ src/Product.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Product {
+
+    private final String name;
+    private ArrayList<Ingredient> ingredients;
+    private PermissibilityStatus status = PermissibilityStatus.HARAM;
+
+    public Product(String name, ArrayList<Ingredient> ingredients) {
+        this.name = name.toLowerCase();
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
+
+    public void removeIngredient(Ingredient ingredient) {
+        this.ingredients.remove(ingredient);
+    }
+
+    public PermissibilityStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PermissibilityStatus status) {
+        this.status = status;
+    }
+
+    public boolean isHalal() {
+        return this.status == PermissibilityStatus.HALAL;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " - " + status;
+    }
+
+}
